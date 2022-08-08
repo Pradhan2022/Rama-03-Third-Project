2ac05640b5525d712c5506ecc6e62f7a0af06af9
@Pradhan2022 <rameswarpradhan2019@gmail.com>
2022-08-08 14:03:48 +0530

    Third Project added to git repository

---
+++ src/test/java/com/Rama/GitThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
